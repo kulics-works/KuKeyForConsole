e513bebaa3cfd4ea32909dfe3f956837b09b8f68
hwdef <hwdef97@gmail.com>
2020-03-10 17:23:50 +0800

    create entrance

---
+++ KuKeyForConsole/KeyService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KuKeyForConsole
+{
+    class KeyService
+    {
+        public static void CreateKey(string MainKey)
+        {
+
+        }
+        public static void DeleteKey(string MainKey)
+        {
+
+        }
+        public static void QueryKey(string MainKey)
+        {
+
+        }
+        public static void UpdateKey(string MainKey)
+        {
+
+        }
+    }
+}
